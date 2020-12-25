d5037c22b1836a559c2e13b8920f64b5aeb2dd59
Arun Ramamurthy <arunramamurthy@Aruns-MBP.home>
2020-12-24 19:33:59 -0500

    Trying to learn CSS - Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Header extends React.Component{
+    render(){
+        return(
+        <header class="header-wrapper">Header
+            <section class="profile-wrapper"> 
+                <div class="profile-img">P</div>
+            </section>
+        </header>
+        );
+    }
+}
+
+export default Header;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import QRURL from '../../components/qrcode';
+import Header from '../../components/Header';
 
 class App extends Component {
     render(){
-        return (<div>
+        return (
+        <div>
+            <Header/>
             Code Goes Here
             <QRURL url="www.cnn.com"/>
         </div>);
